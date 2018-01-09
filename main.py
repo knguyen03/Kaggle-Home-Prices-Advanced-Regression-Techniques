de31c3d8de34291c01bb718cb2086e68a415dbac
nilsleh <35272119+nilsleh@users.noreply.github.com>
2018-01-09 14:00:41 -0500

    Add files via upload
    new file with added comment

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ Created on Tue Jan  9 13:11:00 2018
 
 @author:
     Khang Nguyen
-    Nils Lehman
+    Nils Lehmann
 """
 import numpy as np
 import pandas as pd
 
 train = pd.read_csv("train.csv")
+
+#hello this is a new project
