c31dd232dad1cf451d1c0fd465bb18d7a6657fd2
Ken Nguyen <knguyen0310@gmail.com>
2018-01-09 13:53:50 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 13:11:00 2018
+
+@author:
+    Khang Nguyen
+    Nils Lehman
+"""
+import numpy as np
+import pandas as pd
+
+train = pd.read_csv("train.csv")
