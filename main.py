8bb469a9f2d66ab3193b10c0dea43c2b889f7de6
Ken Nguyen <knguyen0310@gmail.com>
2018-01-10 00:28:41 -0500

    Added regression and submission
    Used all the variables that required no manipulations or preparations. Produced a submission file from the regression but somehow got an error message when I tried to upload it on kaggle

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
+holdout = pd.read_csv("test.csv")
 
 def create_dummies(df,column_name):
     dummies = pd.get_dummies(df[column_name],prefix=column_name)
@@ -21,17 +21,19 @@ def create_dummies(df,column_name):
     return df
 
 train = create_dummies(train,"MSSubClass")
-test = create_dummies(test,"MSSubClass")
-#intialize LinearRegression object
+holdout = create_dummies(holdout,"MSSubClass")
+
 lr = LinearRegression()
 
-columns = ["GrLivArea"]
+columns = ["GrLivArea", "LotArea","1stFlrSF", "2ndFlrSF", "OverallQual",
+           "OverallCond", "FullBath","HalfBath", "BedroomAbvGr", "TotRmsAbvGrd",
+           "WoodDeckSF","OpenPorchSF","YearBuilt","YearRemodAdd"]
 
 all_X = train[columns]
 all_y = train['SalePrice'].reshape(1460,1)
 
 train_X, test_X, train_y, test_y = train_test_split(
-    all_X, all_y, test_size=0.2,random_state=0)
+        all_X, all_y, test_size=0.2,random_state=0)
 
 lr.fit(train_X,train_y)
 
@@ -41,10 +43,26 @@ plt.plot(train_y)
 plt.plot(pred)
 plt.legend(['data','prediction'])
 plt.show()
-plt.scatter(train_X,train_y)
-plt.scatter(train_X,pred)
-plt.legend(['data','fit line'])
-plt.show()
+
+realLr = LinearRegression()
+realLr.fit(all_X,all_y)
+
+all_X_holdout = holdout[columns]
+
+holdout_predictions = realLr.predict(all_X_holdout).ravel()
+holdout_ids = holdout["Id"]
+submission_df = { "Id": holdout_ids,"SalePrice": holdout_predictions}
+#s = pd.Series(holdout_predictions,name='SalePrice')
+#s.index.name = 'Id'
+
+#s.reset_index
+submission = pd.DataFrame.from_dict(submission_df)
+submission.to_csv('submission.csv',index=False)
+
+#plt.scatter(train_X,train_y)
+#plt.scatter(train_X,pred)
+#plt.legend(['data','fit line'])
+#plt.show()
 
     
     
