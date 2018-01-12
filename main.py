5b1079929e0023766ce4bfe5a1ed9f629544797c
Ken Nguyen <knguyen0310@gmail.com>
2018-01-12 17:02:51 -0500

    What we did today
    Created whole bunch of dummies, took out some outliers, and created log terms for 'OverallQual' and 'YearBuilt'

--- main.py
+++ main.py
@@ -9,25 +9,63 @@ Created on Tue Jan  9 13:11:00 2018
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+import seaborn as sns
+from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 
 train = pd.read_csv("train.csv")
 holdout = pd.read_csv("test.csv")
+train = train[train.Id  != 186]
+train = train[train.Id  != 524]
+train = train[train.Id  != 1299]
+train['OverallQual_log']=np.log(train['OverallQual'])
+train['YearBuilt_log']=np.log(train['YearBuilt'])
+holdout['OverallQual_log']=np.log(holdout['OverallQual'])
+holdout['YearBuilt_log']=np.log(holdout['YearBuilt'])
+holdout['GarageArea'].fillna(holdout['GarageArea'].mean(),inplace=True)
+holdout['TotalBsmtSF'].fillna(holdout['TotalBsmtSF'].mean(),inplace=True)
 
 def create_dummies(df,column_name):
     dummies = pd.get_dummies(df[column_name],prefix=column_name)
     df = pd.concat([df,dummies],axis=1)
     return df
 
-train = create_dummies(train,"MSSubClass")
-holdout = create_dummies(holdout,"MSSubClass")
+cols = ['MSZoning','Condition1','Neighborhood','Street','LotConfig','BldgType','CentralAir']
+for c in cols:
+    train = create_dummies(train, c)
+    holdout = create_dummies(holdout, c)
 
-columns = ["GrLivArea", "LotArea","1stFlrSF", "2ndFlrSF", "OverallQual",
-           "OverallCond", "FullBath","HalfBath", "BedroomAbvGr", "TotRmsAbvGrd",
-           "WoodDeckSF","OpenPorchSF","YearBuilt","YearRemodAdd"]
+columns = ['OverallQual', 'GrLivArea', 'GarageArea', 'TotalBsmtSF','FullBath',
+           'YearBuilt',
+           
+           'MSZoning_C (all)', 'MSZoning_FV', 'MSZoning_RH', 'MSZoning_RL', 'MSZoning_RM',
+           
+           'Neighborhood_Blmngtn', 'Neighborhood_Blueste', 'Neighborhood_BrDale',
+           'Neighborhood_BrkSide', 'Neighborhood_ClearCr','Neighborhood_CollgCr', 
+           'Neighborhood_Crawfor', 'Neighborhood_Edwards','Neighborhood_Gilbert', 
+           'Neighborhood_IDOTRR', 'Neighborhood_MeadowV','Neighborhood_Mitchel', 
+           'Neighborhood_NAmes', 'Neighborhood_NPkVill','Neighborhood_NWAmes',
+           'Neighborhood_NoRidge', 'Neighborhood_NridgHt', 'Neighborhood_OldTown',
+           'Neighborhood_SWISU', 'Neighborhood_Sawyer', 'Neighborhood_SawyerW',
+           'Neighborhood_Somerst', 'Neighborhood_StoneBr', 'Neighborhood_Timber',
+           'Neighborhood_Veenker',
+           
+           'Condition1_Artery', 'Condition1_Feedr',
+           'Condition1_Norm', 'Condition1_PosA', 'Condition1_PosN','Condition1_RRAe', 'Condition1_RRAn', 'Condition1_RRNe',
+           'Condition1_RRNn',
+           
+           'Street_Grvl', 'Street_Pave', 
+           
+           'LotConfig_Corner', 'LotConfig_CulDSac', 'LotConfig_FR2', 
+           'LotConfig_FR3', 'LotConfig_Inside',
+           
+           'BldgType_1Fam', 'BldgType_2fmCon', 'BldgType_Duplex', 
+           'BldgType_Twnhs', 'BldgType_TwnhsE',
+           'CentralAir_N', 'CentralAir_Y'
+           ]
+
 all_X = train[columns]
-all_y = train['SalePrice'].reshape(1460,1)
+all_y = train['SalePrice']
 
 #split the training data set so we can test
 
@@ -36,23 +74,16 @@ train_X, test_X, train_y, test_y = train_test_split(
 
 #train model using the train portion of the training data set
 
-lr = LinearRegression()
+lr = linear_model.Ridge(alpha=.5)
 lr.fit(train_X,train_y)
 
 #apply trained model to the test portion of the training data set
 
 pred = lr.predict(train_X)
 
-#output chart
-
-plt.plot(train_y)
-plt.plot(pred)
-plt.legend(['data','prediction'])
-plt.show()
-
 #train model using all of training data set
 
-realLr = LinearRegression()
+realLr = linear_model.Lasso()
 realLr.fit(all_X,all_y)
 
 #apply trained model to test data set
@@ -67,5 +98,4 @@ submission_df = { "Id": holdout_ids,"SalePrice": holdout_predictions}
 submission = pd.DataFrame.from_dict(submission_df)
 submission.to_csv('submission.csv',index=False)
 
-    
-    
+
